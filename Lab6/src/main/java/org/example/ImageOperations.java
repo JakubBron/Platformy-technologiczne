c244dbad3939dd2908c5e033518dd1279fbcbf07
JakubBron <jakub.bronowski1@gmail.com>
2024-04-09 13:42:59 +0200

    New solution.

---
+++ Lab6/src/main/java/org/example/ImageOperations.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ImageOperations {
+
+    public static Pair<String, BufferedImage> loadImagePair(Path path) throws UncheckedIOException {
+        log("Loading image: " + path);
+        try {
+            var filename = path.getFileName().toString();
+            var image = ImageIO.read(path.toFile());
+            return Pair.of(filename, image);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Error while loading image", e);
+        }
+    }
+
+    public static Pair<String, BufferedImage> modifyImagePair(Pair<String, BufferedImage> input) {
+        log("Modifying image: " + input.getLeft());
+        BufferedImage img = input.getRight();
+        BufferedImage result = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+        for (int x = 0; x < img.getWidth(); x++) {
+            for (int y = 0; y < img.getHeight(); y++) {
+                var color = new Color(img.getRGB(x, y));
+                result.setRGB(x, y, new Color(color.getRed(), color.getGreen(), 0).getRGB());
+            }
+        }
+        return Pair.of(input.getLeft(), result);
+    }
+
+    public static void saveImagePair(Pair<String, BufferedImage> pair, Path outputPath) {
+        log("Saving image: " + pair.getLeft());
+        var img = pair.getRight();
+        try {
+            if (!Files.exists(outputPath)) {
+                Files.createDirectories(outputPath);
+            }
+            ImageIO.write(pair.getRight(), "jpg", outputPath.resolve(pair.getLeft()).toFile());
+            log("Image saved: " + pair.getLeft());
+        } catch (IOException e) {
+            throw new UncheckedIOException("Error while saving image", e);
+        }
+    }
+
+    private static void log(String message) {
+        System.out.println("[Thread " + Thread.currentThread().threadId() + "] " + message);
+    }
+}
--- Lab6/src/main/java/org/example/Main.java
+++ Lab6/src/main/java/org/example/Main.java
@@ -1,80 +1,40 @@
 package org.example;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
-import java.util.Objects;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
 
 public class Main {
-    private static Image loadImage(Path path) {
-        try {
-            return new Image(path.getFileName().toString(), ImageIO.read(path.toFile()));
-        } catch (IOException e) {
-            System.err.println("An error occurred: " + e.getMessage());
-            return null;
-        }
-    }
-
-    private static BufferedImage processImage(BufferedImage img) {
-        var width = img.getWidth();
-        var height = img.getHeight();
-        var result = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-        for (var x = 0; x < width; x++) {
-            for (var y = 0; y < height; y++) {
-                var color = new Color(img.getRGB(x, y));
-                result.setRGB(x, y, new Color(color.getRed(), color.getGreen(), 0).getRGB());
-            }
-        }
-        return result;
-    }
-    public static void saveImage(Image img, Path outputDir) {
-        try {
-            ImageIO.write(img.data(), "jpg", outputDir.resolve(img.name()).toFile());
-            System.out.println("Image " + img.name() + " processed & saved.");
-        } catch (IOException e) {
-            System.err.println("An error occurred: " + e.getMessage());
-        }
-    }
     public static void main(String[] args) {
-        if (args.length < 3) {
-            System.err.println("Usage: java -jar <jar-file> <input-dir> <output-dir> <number-of-threads>");
-            System.exit(1);
+        var inputPathStr = args.length >=1 ? args[0] : "./images";
+        var outputPathStr = args.length >=2 ? args[1] : "./images-out";
+        var threads = args.length >=3 ? Integer.parseInt(args[2]) : Runtime.getRuntime().availableProcessors();
+
+        System.out.println("Input path: " + inputPathStr + ", output path: " + outputPathStr + ", threads: " + threads);
+
+        var inputPath = Path.of(inputPathStr);
+        var outputPath = Path.of(outputPathStr);
+
+        List<Path> files;
+        try (var stream = Files.list(inputPath)) {
+            files = stream.collect(Collectors.toList());
+        } catch (Exception e) {
+            System.out.println("Error while reading files!");
+            return;
         }
 
-        try (var stream = Files.list(Path.of(args[0]))) {
-            // check if folder exists
-            if (Files.exists(Path.of(args[1]))) {
-                Files.walk(Path.of(args[1]))
-                        .sorted(Comparator.reverseOrder())
-                        .forEach(path -> {
-                            try {
-                                System.out.println("Deleting: " + path);
-                                Files.delete(path);  //delete each file or directory
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        });
-            }
-            Files.createDirectory(Path.of(args[1]));
-            long time = System.currentTimeMillis();
-            // setting number of threads
-            System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", args[2]);
-
-            stream  .parallel()
-                    .filter(path -> path.toString().endsWith(".jpg"))
-                    .map(Main::loadImage)
-                    .filter(Objects::nonNull)
-                    .map(img -> new Image(img.name(), processImage(img.data())))
-                    .forEach(img -> saveImage(img, Path.of(args[1])));
-
-            System.out.println("Execution time: " + (System.currentTimeMillis() - time) + " ms");
-        } catch (IOException ex) {
-            System.err.println("An error occurred: " + ex.getMessage());
+        var parallelStream = files.parallelStream();
+        long startTime = System.currentTimeMillis();
+        try(ForkJoinPool threadPool = new ForkJoinPool(threads)) {
+            threadPool.submit(
+                    () -> parallelStream.map(ImageOperations::loadImagePair)
+                    .map(ImageOperations::modifyImagePair)
+                    .forEach(pair -> ImageOperations.saveImagePair(pair, outputPath)));
         }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken: " + (endTime - startTime) + " ms");
     }
+
 }
