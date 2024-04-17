9214ee48468e5de08617d4279eef9ef573678d33
Jakub Bronowski <164743955+JakubBron@users.noreply.github.com>
2024-04-17 07:40:58 +0200

    Small changes in printing & showing oldest file
    Added via upload.

---
+++ lab7/lab7/Comparator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7
+{
+    [Serializable]
+    public class Comparator : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x.Length == y.Length)
+            {
+                return string.Compare(x, y, StringComparison.Ordinal);
+            }
+            return x.Length > y.Length ? 1 : -1;
+        }
+    }
+}
