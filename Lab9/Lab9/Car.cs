62697e6c405f77723b6f047098bfcb9ef9701e38
JakubBron <jakub.bronowski1@gmail.com>
2024-05-05 12:46:33 +0200

    Task 9 completed.

---
+++ Lab9/Lab9/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Lab9
+{
+    [XmlType("car")]           // changes in XML phrase "Car" to "car"
+    public class Car
+    {
+        public string model;
+        public int year;
+        [XmlElement(ElementName = "engine")]
+        public Engine motor { get; set; } // getters & setters will be avaliable without need to implement it
+
+        public Car()
+        {
+            
+        }
+
+        public Car(string modelName, Engine motorOnBoard, int yearOfProduction)
+        {
+            model = modelName;
+            year = yearOfProduction;
+            motor = motorOnBoard;
+        }
+
+        public void setModel(string modelName)
+        {
+            model = modelName;
+        }
+        
+        public string getModel()
+        {
+            return model;
+        }
+
+        public void setYear(int yearOfProduction)
+        {
+            year = yearOfProduction;
+        }
+
+        public int getYear()
+        {
+            return year;
+        }
+    }
+}
---
+++ Lab9/Lab9/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text;
+
+namespace Lab9
+{
+    [XmlRoot(ElementName = "engine")]
+    public class Engine
+    {
+        public double displacement { get; set; }
+        public double horsePower { get; set; }
+        [XmlAttribute]
+        public string model { get; set; }
+
+        public Engine() 
+        { 
+        
+        }
+
+        public Engine(double Displacement, double HorsePower, string Model)
+        {
+            displacement = Displacement;
+            horsePower = HorsePower;
+            model = Model;
+        }
+
+    }
+}
